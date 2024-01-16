88b4a80b004f3f08b75c2b312dfaf10b80415e55
Laura Pečnik Budna <laura.budna@gmail.com>
2024-01-16 15:09:15 +0100

    add javascript tutorial

---
+++ javascript-tutorial/main.js
@@ -0,0 +1,72 @@
+// console.log('Hello from main.js')
+// // comment
+// // ctrl+?
+
+// // variables
+// let age = 25
+// console.log(age)
+
+// // constant
+// const salary = 80000
+// console.log(salary)
+
+// let sum = 0
+// sum = 5
+// console.log(sum)
+
+// TYPES
+// string
+const name = 'Laura'
+const surname = "Budna"
+const job = `student`
+
+// // float
+// const total = 0
+// const PI = 3.14
+
+// // bool
+// const isPrimaryNumber = true
+// const isNew = false
+
+// // undefined
+// let result
+// const res = undefined
+
+// // null
+// const data = null
+
+// // bigint, symbol
+
+// // NON PREEMITIVE TYPE
+// // object - comblex data type
+// const person = {
+//     firstName: 'Bruce',
+//     lastName: 'Novak',
+//     age: 30
+// }
+
+// console.log(person.firstName)
+
+// //array
+// const oddNumbers = [1, 3, 5, 7, 9]
+// console.log(oddNumbers[0])
+
+// // data types are converted by themselves
+// let a = 10
+// a = 'Laura'
+// a = true
+// console.log(a) 
+// // output: true
+
+// OPERATORS
+// assignment operators - assign values to operators
+let x = 10
+let y = 5
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+
+
+
