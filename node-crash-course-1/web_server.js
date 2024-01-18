3df94c94409d88367345981a50c033129b67ca7d
Laura Pečnik Budna <laura.budna@gmail.com>
2024-01-18 09:41:23 +0100

    add node.js: server creation

--- node-crash-course-1/web_server.js
+++ node-crash-course-1/web_server.js
@@ -1,11 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((request, response) => {
+    console.log(request.url, request.method);
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html");
-    response.end("<h1>Hello World</h1>");
+    fs.readFile("./index.html", (err, data) => {
+        if (err) {
+            console.error(err);
+            response.end();
+        } else {
+            response.end(data);
+        }
+    });
 });
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
