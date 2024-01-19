6337ed3116785cfc9f663e263996ca51c1bb2152
Laura Pečnik Budna <laura.budna@gmail.com>
2024-01-19 14:09:58 +0100

    add routes tutorial

--- node-middleware/express_intro-main/server.js
+++ node-middleware/express_intro-main/server.js
@@ -33,47 +33,42 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 // serve static files (like html)
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
-});
-
-// Route handlers
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
-});
+// routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
 
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
+// // Route handlers
+// app.get('/hello(.html)?', (req, res, next) => {
+//     console.log('attempted to load hello.html');
+//     next()
+// }, (req, res) => {
+//     res.send('Hello World!');
+// });
 
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
 
-app.get('/chain(.html)?', [one, two, three]);
+// // chaining route handlers
+// const one = (req, res, next) => {
+//     console.log('one');
+//     next();
+// }
+
+// const two = (req, res, next) => {
+//     console.log('two');
+//     next();
+// }
+
+// const three = (req, res) => {
+//     console.log('three');
+//     res.send('Finished!');
+// }
+
+// app.get('/chain(.html)?', [one, two, three]);
 
 
 // app.use('/') -> for middleware
