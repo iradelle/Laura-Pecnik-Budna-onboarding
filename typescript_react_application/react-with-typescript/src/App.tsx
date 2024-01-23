0851669ded01806c75578d24449560d402967c1d
Laura Pečnik Budna <laura.budna@gmail.com>
2024-01-23 12:28:06 +0100

    add react w/typescript application start

---
+++ typescript_react_application/react-with-typescript/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const [number, setNumber] = useState(5); /*< number | string > number or string*/
+
+  const changeNumber = () => {
+    setNumber(10)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
