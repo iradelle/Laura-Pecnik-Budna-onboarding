5c133c993446795c7e2247ff3ecb6db5bef1c3f5
Laura Pečnik Budna <laura.budna@gmail.com>
2024-01-24 21:08:13 +0100

    add getBoardById and deleteBoard functionality

--- nest-js-tutorial/src/boards/boards.controller.ts
+++ nest-js-tutorial/src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { title } from 'process';
@@ -22,6 +22,16 @@ export class BoardsController {
         @Body() CreateBoardDto: CreateBoardDto
     ) :Board {
         return this.boardsService.createBoard(CreateBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id:string): Board {
+        return this.boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id:string): void {
+        this.boardsService.deleteBoard(id);
     }
 
 }
--- nest-js-tutorial/src/boards/boards.service.ts
+++ nest-js-tutorial/src/boards/boards.service.ts
@@ -26,4 +26,12 @@ export class BoardsService {
         this.boards.push(board);
         return board;
     }
+
+    getBoardById(id: string): Board {
+        return this.boards.find((board) => board.id === id);
+    }
+
+    deleteBoard(id: string): void {
+        this.boards = this.boards.filter(board => board.id !== id);
+    }
 }
