30f3b45722f9e1bf5e9e4c3d80118c8853948118
Laura Pečnik Budna <laura.budna@gmail.com>
2024-01-25 12:15:24 +0100

    add connect to database

--- nest-js-tutorial/src/app.module.ts
+++ nest-js-tutorial/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule],
   
   
 })
