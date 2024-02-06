5bd1f839f0abc5594eb90862f76a3c0641479fdb
Laura Pečnik Budna <laura.budna@gmail.com>
2024-02-06 12:06:16 +0100

    import usersmodule

--- SkillUpMentor-tutorials/skillupmentor-tutorial-backend-master/src/modules/app.module.ts
+++ SkillUpMentor-tutorials/skillupmentor-tutorial-backend-master/src/modules/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { configValidationSchema } from 'config/schema.config';
 import { DatabaseModule } from './database/database.module';
 import { LoggerMiddleware } from 'middleware/logger.middleware';
+import { UsersModule } from 'users/users.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { LoggerMiddleware } from 'middleware/logger.middleware';
       validationSchema: configValidationSchema,
     }),
     DatabaseModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
